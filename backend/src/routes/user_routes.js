96193dcb822b25f9ef5da741d3a6402e0020e624
Pawan Mahajan <pawanmahajan2029@gmail.com>
2025-12-11 20:02:24 +0530

    Initial commit: BaatLink backend project

---
+++ backend/src/routes/user_routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { login,register } from "../controllers/user_controller.js";
+
+
+
+
+
+
+const router = Router();
+router.route("/login").post(login)
+router.route("/register").post(register)
+router.route("/add_to_activity")
+router.route("/get_all_activity")
+
+
+export default router;
